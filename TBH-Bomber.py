1074c0330b9ee566fbeeb936f692c7ef99027c14
Kali-Linux20 <139705370+Kali-Linux20@users.noreply.github.com>
2024-01-24 13:25:47 +0600

    Update TBH-Bomber.py

--- TBH-Bomber.py
+++ TBH-Bomber.py
@@ -1,3 +1,5 @@
+
+#---------------import------------------#
 import  os,sys,time,requests,random,string
 import shutil
 from requests.structures import CaseInsensitiveDict
@@ -8,7 +10,7 @@ RED = '\033[1;31m'
 WHITE = '\033[1;97m'
 GREEN = '\033[1;32m' 
 sai="\033[1;30m"#à¦›à¦¾à¦‡
-YELLOW = '\033[1;33m'
+YELLOW = '\033[1;33m'you
 BLUE = '\033[1;34m'
 perpal = '\033[1;35m'
 crim='\033[1;36m'
@@ -33,27 +35,23 @@ except (requests.ConnectionError, requests.Timeout) as exception:
  print("\n\033[1;37m[\033[1;32m#\033[1;37m] \033[1;31m Your Internet Connection Is Poor !")
  time.sleep(3)
 #--------------- check update ----------------------#
+
 def update():
 	toolVersion=1.1
+	mainVersion = requests.get('https://github.com/Kali-Linux20/Update/blob/main/Bomber.txt').text
 	try:
-		parsedData = requests.get("https://github.com/Kali-Linux20/Update-2/blob/main/.%20bomber").json()
+		if toolVersion in mainVersion:
+			pass
+		else:
+			slowprint("\n    \033[92m[\033[37m!\033[92m] \033[37mTool Update Found!")
+			time.sleep(0.5)
+			slowprint("    \033[92m[\033[37m!\033[92m] \033[37mUpdating Tool: ", end="")
+			os.system("cd .. && rm -rf TBH-Bomber && git clone https://github.com/Kali-Linux20/TBH-Bomber > /dev/null 2>&1")
+			print("\033[37mDone")
+			slowprint("\n    \033[92m[\033[37m*\033[92m] \033[37mStarting Tool...")
+			time.sleep(0.8)
+			os.system("cd .. && cd TBH-Bomber && python TBH-Bomber.py")
 	except:
-		print("\n    \033[92m[\033[91m!\033[92m] \033[37mPlease Connect To The Internet!")
-		time.sleep(1)
-		l = input("\n    \033[92m[\033[37m*\033[92m] \033[37mPress Enter To Continue...")
-		update()
-	mainVersion = parsedData["version"]
-	if (toolVersion != mainVersion):
-		slowprint("\n    \033[92m[\033[37m!\033[92m] \033[37mTool Update Found!")
-		time.sleep(0.5)
-		slowprint("    \033[92m[\033[37m!\033[92m] \033[37mUpdating Tool: ", end="")
-		os.system("cd .. && rm -rf TBH-Bomber && git clone https://github.com/Kali-Linux20/TBH-Bomber > /dev/null 2>&1")
-		print("\033[37mDone")
-		slowprint("\n    \033[92m[\033[37m*\033[92m] \033[37mStarting Tool...")
-		time.sleep(0.8)
-		os.system("cd .. && cd TBH-Bomber && python TBH-Bomber.py")
-
-update()
-print("kali is king")
-print("kali is king")
-print("kali is king")
+		pass
+		
+print("kali is king")￼Enter
